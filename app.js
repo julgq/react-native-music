93392c6131e9239af4e5e27f35ecd6d9a1718a96
Julio <Julio@iMac-de-Julio.local>
2017-09-15 13:13:45 -0600

    ios format initial

--- app.js
+++ app.js
@@ -10,15 +10,36 @@ import {
   StyleSheet,
   Text,
   View,
+  Image,
 } from 'react-native';
 
-export default class PlatziMusic extends Component {
-  
+import Icon from 'react-native-vector-icons/Ionicons'
 
+export default class PlatziMusic extends Component {
   render() {
+    const image = 'https://lastfm-img2.akamaized.net/i/u/300x300/31a51f6e3ec647c8997150ec837891c7.png'
+    const name = 'David Bowie'
+    const likes = 200
+    const comments = 140
+
     return (
       <View style={styles.container}>
-        
+        <View style={styles.artistBox}>
+          <Image style={styles.image} source={{ uri: image }} />
+          <View style={styles.info}>
+            <Text style={styles.name}>{name}</Text>
+            <View style={styles.row}>
+              <View style={styles.iconContainer}>
+                <Icon name="ios-heart-outline" size={30} color="gray" />
+                <Text style={styles.count}>{likes}</Text>
+              </View>
+              <View style={styles.iconContainer}>
+                <Icon name="ios-chatboxes-outline" size={30} color="gray" />
+                <Text style={styles.count}>{comments}</Text>
+              </View>
+            </View>
+          </View>
+        </View>
       </View>
     );
   }
@@ -26,12 +47,41 @@ export default class PlatziMusic extends Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1, // Ocupa toda la pantalla
-    flexDirection: 'row',
-    backgroundColor: 'red',
-    flexWrap: 'wrap' //  Para lograr que los elementos caigan cuando sobre pasa el tamaño
+    flex: 1,
+    backgroundColor: 'lightgray',
+    paddingTop: 50,
   },
- 
+  artistBox: {
+    backgroundColor: 'white',
+    flexDirection: 'row'
+  },
+  image: {
+    width: 150,
+    height: 150,
+  },
+  info: {
+    flex: 1,
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  name: {
+    fontSize: 20,
+    marginTop: 10,
+    color: '#333'
+  },
+  row: {
+    flexDirection: 'row',
+    marginHorizontal: 30,
+    marginTop: 15
+  },
+  iconContainer: {
+    flex: 1,
+    alignItems: 'center'
+  },
+  count: {
+    color: 'gray'
+  }
 });
 
 AppRegistry.registerComponent('PlatziMusic', () => PlatziMusic);
